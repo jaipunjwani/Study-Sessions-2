2ce44107d714b53c4c016c6cfe664e4964ebd8fa
jaipunjwani <jaipunjwani@mail.adelphi.edu>
2016-05-22 02:08:04 +0000

    improved UI, changed labels, added validation

--- app/models/studysession.rb
+++ app/models/studysession.rb
@@ -1,4 +1,6 @@
 class Studysession < ActiveRecord::Base
+    validates :subject, presence: true
+    validates :location, presence: true
     has_many :sessionmembers
     has_many :users, through: :sessionmembers
 end
